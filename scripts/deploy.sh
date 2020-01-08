07f00622a8dc5018dc661a3e52d73e577e0e7b19
darkhorizon12 <kimjuon12@gmail.com>
2020-01-08 10:15:46 +0900

    띄어쓰기 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,13 +26,10 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "======== JAR NAME: $JAR_NAME"
 
-echo "======== $JAR_NAME에 실행권한 추가"
+echo "======== $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
 echo "======== $JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=classpath:/application.yml,classpath:/application-real.properties,\
-      /home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-      -Dspring.profiles.active=real \
-      $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.yml,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
