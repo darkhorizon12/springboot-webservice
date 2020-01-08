5739561a381c5b492e226b807dcc65503de0fa3b
darkhorizon12 <kimjuon12@gmail.com>
2020-01-08 16:37:07 +0900

    무중단배포를 위한 쉘스크립트파일 및 빌드파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,18 +5,17 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot-webservice
+PROJECT_NAME=freelec-springboot2-webservice
 
-echo "> 빌드 파일 복사"
+echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR NAME: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
@@ -26,7 +25,7 @@ echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> $JAR_NAME 을 profile=$IDLE_PROFILE 로 실행"
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
       -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
