c6a8a858fc3511e12517241c9714beee3656284e
darkhorizon12 <kimjuon12@gmail.com>
2020-01-09 14:35:57 +0900

    CD를 위한 테스트 커밋

--- src/main/java/com/matey/bootwebservice/config/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/matey/bootwebservice/config/oauth/CustomOAuth2UserService.java
@@ -31,12 +31,16 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         OAuth2UserService delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
+        // 현재 로그인 진행 중인 서비스 구분값(google, naver ..)
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        // OAuth2 로그인 진행시 키가 되는 필드값
+        // 구글의 경우 “sub”로 기본지원. 네이버, 카카오 등은 지원안함
         String userNameAttributeName = userRequest
                 .getClientRegistration()
                 .getProviderDetails()
                 .getUserInfoEndpoint()
                 .getUserNameAttributeName();
+        // OAuth2UserService를 통해 가져온 OAuth2User의 속성값을 담은 클래스
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
